b42ee78a70ffd62fcc6d61103637f373f627fb01
Sebastian Jelonek <jeloneksebastian93@gmail.com>
2025-07-09 11:00:51 +0200

    added first handler to the server

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -15,4 +17,19 @@ func main() {
 
 	API_KEY := os.Getenv("IDOSELL_API_KEY")
 	log.Println(API_KEY)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var id int
+		id++
+
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"Status":  "Success",
+			"Message": "Root",
+			"id":      id,
+		})
+	})
+
+	PORT := os.Getenv("PORT")
+	log.Printf("starting server at port %s", PORT)
+	http.ListenAndServe(":"+PORT, nil)
 }
